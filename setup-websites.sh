52a63b50e0d86688be2349f3da90eb577cbb62eb
Jacobchestnut16 <88856888+Jacobchestnut16@users.noreply.github.com>
2023-12-09 22:46:20 -0500

    Update setup-websites.sh

--- setup-websites.sh
+++ setup-websites.sh
@@ -1,55 +1,65 @@
 #!/bin/bash
 
-# Parse command-line options
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    -sites|--sites)
-      shift
-      siteNames=($1)
-      ;;
-    *)
-      echo "Unknown parameter passed: $1"
-      exit 1
-      ;;
-  esac
-  shift
-done
+# Function to create a new website
+create_website() {
+    # Get information from user
+    read -p "Enter the domain name for the website (e.g., example.com): " domain
 
-# Install Apache
-sudo apt update
-sudo apt install -y apache2
+    # Validate domain name
+    if [[ ! $domain =~ ^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+        echo "Invalid domain name. Please enter a valid domain name."
+        exit 1
+    fi
 
-# Create website directories and set permissions
-for site in "${sites[@]}"; do
-    sudo mkdir "/var/www/${site}"
-    sudo chown -R www-data:www-data "/var/www/${site}"
-    echo "${site}" | sudo tee "/var/www/${site}/index.html" > /dev/null
-done
-
-# Create virtual host configuration files
-for site in "${sites[@]}"; do
-    sudo tee "/etc/apache2/sites-available/${site}.conf" > /dev/null <<EOF
+    # Set up Apache virtual host
+    cat <<EOF | sudo tee /etc/apache2/sites-available/$domain.conf > /dev/null
 <VirtualHost *:80>
-    ServerAdmin webmaster@${site}
-    ServerName ${site}
-    DocumentRoot /var/www/${site}
+    ServerAdmin webmaster@$domain
+    ServerName $domain
+    DocumentRoot /var/www/$domain
+
     ErrorLog \${APACHE_LOG_DIR}/error.log
     CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+    <Directory /var/www/$domain>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
 </VirtualHost>
 EOF
-done
 
-# Enable virtual hosts
-for site in "${sites[@]}"; do
-    sudo a2ensite "${site}.conf"
-done
+    # Enable the virtual host
+    sudo a2ensite $domain.conf
 
-# Restart Apache
-sudo service apache2 restart
+    # Reload Apache
+    sudo systemctl reload apache2
 
-# Update hosts file
-for site in "${sites[@]}"; do
-    echo "127.0.0.1 ${site}" | sudo tee -a /etc/hosts > /dev/null
-done
+    # Create an index.html file for testing
+    echo "<html><body><h1>Welcome to $domain!</h1></body></html>" | sudo tee /var/www/$domain/index.html > /dev/null
 
-echo "Websites set up successfully!"
+    # Display information to the user
+    echo "Website $domain created successfully."
+    echo "Document root: /var/www/$domain"
+}
+
+# Check if Apache is installed
+if ! command -v apache2 &> /dev/null; then
+    echo "Apache is not installed. Please install Apache and run the script again."
+    exit 1
+fi
+
+# Check if MySQL is installed
+if ! command -v mysql &> /dev/null; then
+    echo "MySQL is not installed. Please install MySQL and run the script again."
+    exit 1
+fi
+
+# Check if PHP is installed
+if ! command -v php &> /dev/null; then
+    echo "PHP is not installed. Please install PHP and run the script again."
+    exit 1
+fi
+
+# Call the function to create a new website
+create_website
